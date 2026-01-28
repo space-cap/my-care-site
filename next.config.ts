2b5f918e50b4a6ee6ab587b9b300dd59e90e6397
이영현 <hong.git@yahoo.com>
2026-01-28 15:53:27 +0900

    굿

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from "next"
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
+  turbopack: {
+    root: "C:\\workdir\\space-cap\\my-care-site",
+  },
+}
 
-export default nextConfig;
+export default nextConfig
